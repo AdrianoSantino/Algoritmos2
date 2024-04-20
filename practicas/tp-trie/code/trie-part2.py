42e58aef15f7ed0cf64abedaa8a42629c9ecb00e
AdrianoSantino <adrianosantinof@gmail.com>
2024-04-20 16:16:49 -0300

    Added a hasReversedStrings function, tried 5hs+ to accomplish it but failed, used ChatGPT instead. Conclusion I got: its worth learning to use stacks for almost every exercise.

--- practicas/tp-trie/code/trie-part2.py
+++ practicas/tp-trie/code/trie-part2.py
@@ -25,15 +25,15 @@ def sameDocument(T1: Trie, T2: Trie) -> bool:
     El costo es de O(m*n*log(n)), donde m es el número total de nodos y n es el número de hijos por cada nodo m.
     El costo n*log(n) se debe a que cada llamada aplica dos ordenamientos de listas."""
 
-    def sortByAttribute(node):
+    def nodeKey(node):
         return node.key
 
     def sameDocumentR(node1, node2):
         if len(node1.children) != len(  # here are all the logical cases where a doc-node is considered != from another
                 node2.children) or node1.key != node2.key or node1.isEndOfWord != node2.isEndOfWord:
             return False
-        keySorted1 = sorted(node1.children, key=sortByAttribute)
-        keySorted2 = sorted(node2.children, key=sortByAttribute)
+        keySorted1 = sorted(node1.children, key=nodeKey)
+        keySorted2 = sorted(node2.children, key=nodeKey)
         for i in range(len(keySorted1)):  # checks that all nodes satisfy sameDocument condition
             if not sameDocumentR(keySorted1[i], keySorted2[i]):  # only if a False condition is found, return
                 return False
@@ -44,14 +44,23 @@ def sameDocument(T1: Trie, T2: Trie) -> bool:
     return False
 
 
+def hasReversedStrings(T):
+    node = T.root
+    stack = [(node, '')]
+    while stack:  # while there is still a node to be searched
+        node, word = stack.pop()  # bring back the last node
+        if node.isEndOfWord and search(T, word[::-1]):  # when an EOW was found, search its reversed form
+            return True
+        for child in node.children:
+            stack.append((child, word + child.key))  # append a node so to analyze it
+    return False
+
+
 def test():
     T = Trie()
-    for palabra in ["hae", "coro", "cero", "cera"]:
+    for palabra in ["dsa", "asd"]:
         insert(T, palabra)
-    P = Trie()
-    for palabra in ["hae", "coro", "cero", "cera"]:
-        insert(P, palabra)
-    print(sameDocument(T, P))
+    print(hasReversedStrings(T))
 
 
 test()
