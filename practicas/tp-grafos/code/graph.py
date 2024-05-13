0d3943e1b6e6e03ac141ef34467ac5dd8bd3fa08
AdrianoSantino <adrianosantinof@gmail.com>
2024-05-13 15:23:17 -0300

    add graph DatasStructure

--- practicas/tp-grafos/code/graph.py
+++ practicas/tp-grafos/code/graph.py
@@ -1,23 +1,61 @@
 from dictionary import hashf, create
 
 
+class Node:
+    key, next, color = None, None, "WHITE"
+
+    def __repr__(self):
+        return self.key
+
+
+def createNode(key):
+    new = Node()
+    new.key = key
+    return new
+
+
+def appNode(root, key):
+    if not root:
+        return createNode(key)
+    node = root
+    while node.next:
+        node = node.next
+    node.next = createNode(key)
+    return root
+
+
 def createGraph(V, E):
     """O(V + E)"""
     G = create(len(V))
     for v in V:
         index = hashf(v, len(V))
-        G[index] = [v]
+        G[index] = createNode(v)
 
     for a, b in E:
         index = hashf(a, len(V))
-        G[index].append(b)
+        appNode(G[index], b)
+        index = hashf(b, len(V))
+        appNode(G[index], a)
     return G
 
 
 def printGraph(G):
     for v in G:
-        print(v)
+        printList(v)
     print()
+
+
+def printList(root):
+    node = root
+    while node:
+        print(node.key, end=' ')
+        node = node.next
+    print()
+
+
+def startNode(G, letter):
+    index = hashf(letter, len(G))
+    return G[index]
 
 
 if __name__ == "__main__":
@@ -25,4 +63,3 @@ if __name__ == "__main__":
     E = [('u', 'a'), ('w', 'a')]
     G = createGraph(V, E)
     printGraph(G)
-    pass
